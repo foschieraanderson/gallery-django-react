67efd9ccf7e8dc6af944be29ad9e2bb4daa500e8
foschieraanderson <foschieraanderson@gmail.com>
2021-04-24 14:50:06 -0300

    Renomeando imagem do upload

--- upload/models.py
+++ upload/models.py
@@ -1,8 +1,15 @@
+from datetime import datetime
+
 from django.db import models
 
 
+def directory_path(instance, filename):
+    """ Define o diretório a ser salvo e renomeia a imagem """
+    return "uploads/{0}-{1}".format(datetime.now(), filename)
+
+
 class Upload(models.Model):
-    image = models.ImageField("Imagem", upload_to="uploads/", blank=False)
+    image = models.ImageField("Imagem", upload_to=directory_path, blank=False)
     is_available = models.BooleanField("Disponível", default=False)
 
     created = models.DateTimeField("Criado", auto_now_add=True)
